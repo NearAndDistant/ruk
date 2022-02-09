de0e4e3996d1168f70be6294e323ab3bee97089e
Near and Distant <p-corby@hotmail.co.uk>
2022-02-09 17:01:36 +0000

    script upload

--- data_scripts/ons_homeless_deaths/write_csv_script.R
+++ data_scripts/ons_homeless_deaths/write_csv_script.R
@@ -10,5 +10,17 @@ download.file(
 # read xls
 hld_raw <- readxl::read_xlsx(temp_ons, sheet = "Table 7", skip = 3)
 
-write_csv(hld_raw , here::here("hdl_raw.csv"))
+# clean and prepare
+hld_tidy <- 
+  hld_raw %>% 
+  janitor::clean_names() %>% 
+  select(area_code, area_name, x2020_identified_deaths:x2013_identified_deaths) %>%
+  pivot_longer(cols = c(x2020_identified_deaths:x2013_identified_deaths), names_to = "year", values_to = "deaths") %>%
+  mutate(year = str_remove(year, "_identified_deaths"),
+         year = str_remove(year, "x"),
+         year = as.numeric(year)) %>%
+  mutate(deaths = if_else(deaths == 0, NA_real_, deaths)) %>%
+  mutate(label = if_else(deaths == 0, NA_character_, paste0(area_name, " (", deaths, ")")))
+
+write_csv(hld_tidy , here::here("hdl_tidy.csv"))
 
